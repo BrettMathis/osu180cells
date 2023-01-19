c2a87fca43df0954a531ecab7edbd488c1d4b840
Brett Mathis <brett.mathis@okstate.edu>
2023-01-19 10:41:43 -0600

    Added in-progress debug before branch switch

---
+++ runmebeforemake.sh
@@ -0,0 +1,6 @@
+export OPENLANE_ROOT=$(pwd)/dependencies/openlane_src # you need to export this whenever you start a new shell
+
+export PDK_ROOT=$(pwd)/dependencies/pdks # you need to export this whenever you start a new shell
+
+# for the gf180 GFMPW shuttles...
+export PDK=gf180mcuC
